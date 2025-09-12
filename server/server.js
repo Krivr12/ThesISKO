6f96ef73ca0a32706c709f061adefbbfbc405f5e
Anjenggg <galicia.maangela.natano@gmail.com>
2025-09-13 03:30:18 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/records.js";
-import group_progress from "./routes/group_progress.js"
+import records from "./routes/records.js";  
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -28,7 +27,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use("/records", records);
-app.use("/group_progress", group_progress);
 
 app.listen(PORT, () => {
   console.log(`✅ Server listening on port ${PORT}`);
