a09963b73bb48aba280a7cca84d72cb0a6d0a25b
Chris <christopherbryanevagelista@gmail.com>
2025-09-21 23:48:07 +0800

    Adjust angular.json

---
+++ client/.angular/cache/20.3.2/Azalea/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-ERBJAPDC.js";
+import "./chunk-J6FZIA3H.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ client/.angular/cache/20.3.2/Azalea/vite/deps/primeng_button.js
@@ -0,0 +1,29 @@
+import {
+  Button,
+  ButtonClasses,
+  ButtonDirective,
+  ButtonIcon,
+  ButtonLabel,
+  ButtonModule,
+  ButtonStyle
+} from "./chunk-2ZUADST7.js";
+import "./chunk-SS5BXU7U.js";
+import "./chunk-WPPEVNHU.js";
+import "./chunk-3CBZNRDO.js";
+import "./chunk-EODZSSJY.js";
+import "./chunk-R4M5QIKS.js";
+import "./chunk-U2VNPEYD.js";
+import "./chunk-P3D5YQ3N.js";
+import "./chunk-KGBGYEO4.js";
+import "./chunk-ZJ25XCV3.js";
+import "./chunk-J6FZIA3H.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  Button,
+  ButtonClasses,
+  ButtonDirective,
+  ButtonIcon,
+  ButtonLabel,
+  ButtonModule,
+  ButtonStyle
+};
